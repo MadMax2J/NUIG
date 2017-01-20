e9d2dab008f6f19115828fc775772a6a1274602b
John Byrne <usscork@gmail.com>
2017-01-20 16:41:24 +0000

    Initial commit

---
+++ loopAndDebugTestingC/loopAndDebugTestingC/whileLoops.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+
+	int grade;
+	int count = 1;
+	int total = 0;
+	int average;
+
+	while (count <= 10) {
+		printf("What grade did student %d get: ", count);
+		scanf("%d", &grade);
+
+		total += grade;
+		count++;
+	}
+
+	average = total / (count - 1);
+	printf("The class average was %d\%.\n", average);
+
+
+
+
+	return(0);
+}
