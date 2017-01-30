70591c7ad8990c5ed609cdcabe7055c9c9fa3ffa
John Byrne <usscork@gmail.com>
2017-01-30 13:14:44 +0000

    forLoop.c

--- week2_ex1/week2_ex1/earningsCalc.c
+++ week2_ex1/week2_ex1/earningsCalc.c
@@ -1,12 +1,12 @@
 /*
-One large chemical company pays its salespeople on a commission basis. 
-The salespeople receive $200 per week plus 9% of their gross sales for that week. 
-For example, a salesperson who sells $5000 worth of chemicals in a week receives 
-$200 plus 9% of $5000, or a total of $650. 
+One large chemical company pays its salespeople on a commission basis.
+The salespeople receive $200 per week plus 9% of their gross sales for that week.
+For example, a salesperson who sells $5000 worth of chemicals in a week receives
+$200 plus 9% of $5000, or a total of $650.
 
-This program will take as input, each salesperson's gross sales for last week and 
-will calculate and display that salesperson's earnings, processing one 
-salesperson's figures at a time. 
+This program will take as input, each salesperson's gross sales for last week and
+will calculate and display that salesperson's earnings, processing one
+salesperson's figures at a time.
 
 By John Byrne
 */
@@ -21,15 +21,15 @@ int main() {
 	scanf("%f", &sales); //Read-in user input and assign the value to the sales variable.
 
 	while (sales != -1) { //Check for continuation (not termination) condition
-		//Consider handling a negative sales figure input??
+						  //Consider handling a negative sales figure input??
 
 		printf("Salary is: $%.2f\n", 200 + (sales * 0.09)); //Display Salary based on commission.
-		
-		//Prompt user for another sales figure...
+
+															//Prompt user for another sales figure...
 		printf("%s", "Enter sales in dollars (-1 to end): ");
 		scanf("%f", &sales); //Read-in user input and assign the value to the sales variable.
 	} //Jump back to the loop condition
-	
-	//Upon successful completion of the program...
+
+	  //Upon successful completion of the program...
 	return(0);
 }
---
+++ week3_ex1/week3_ex1/forLoops.c
@@ -0,0 +1,54 @@
+/*
+Write 'for' statements that print the following sequences of values:
+
+a. 1, 2, 3, 4, 5, 6, 7
+b. 3, 8, 13, 18, 23
+c. 20, 14, 8, 2, -4, -10
+d. 19, 27, 35, 43, 51
+
+
+by John Byrne - 30th January 2017
+*/
+
+#include <stdio.h>
+
+int main() {
+
+	// Print group 'a.'
+	printf("%s", "a. ");			//Group label
+	for (int a = 1; a <= 7; a++) {	//Set-up 'for' loop to repeat 7 times
+		printf("%5d", a);			//Print the value
+	}								//End of for
+	puts("\n");						//Separate next group with a new line
+	//End of group 'a.'
+
+
+	// Print group 'b.'
+	printf("%s", "b. "); 
+	for (int b = 3; b <= 23; b += 5) {
+		printf("%5d", b);
+	}	//End of for
+	puts("\n");
+	//End of group 'b.'
+
+	
+	// Print group 'c.'
+	printf("%s", "c. ");
+	for (int c = 20; c >= -10; c -= 6) {
+		printf("%5d", c);
+	}	//End of for
+	puts("\n");
+	//End of group 'c.'
+
+	
+	// Print group 'd.'
+	printf("%s", "d. ");
+	for (int d = 19; d <= 51; d += 8) {
+		printf("%5d", d);
+	}	//End of for
+	puts("\n");
+	//End of group 'd.'
+
+
+	return(0); //Assuming successful completion
+} //End of main()
